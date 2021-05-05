60ac7d34b05c1b79c459a4e6b9dabe893bd4dfbb
Abhay5855 <ap3919529@gmail.com>
2021-05-06 00:25:33 +0530

    docs: Using react router dom

--- chat-webapp-react/src/App.js
+++ chat-webapp-react/src/App.js
@@ -1,12 +1,27 @@
-
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./components/Chat";
+import styled from "styled-components";
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Container>
+          <Switch>
+            <Route path="/room">
+              <Chat />
+            </Route>
+            <Route path="/">Login</Route>
+          </Switch>
+        </Container>
+      </Router>
     </div>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
 export default App;
