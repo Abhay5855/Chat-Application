51b4326e7e05f39a5afc603f794f1c38df8044eb
Abhay5855 <ap3919529@gmail.com>
2021-05-06 20:40:43 +0530

    docs: Giving the cards styling if user is present or not

---
+++ chat-webapp-react/src/components/Messages.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card, CardContent, Typography} from "@material-ui/core";
+import "./Message.css";
+function Messages({msg , username}) {
+    const isUser = username === msg.username;
+    return (
+
+        <div className={`msg ${isUser && 'message_card'}`}>
+        <Card className={isUser ? "message_userCard" : "message_guessCard"}>
+
+        <CardContent>
+            <Typography
+            color="white"
+            variant="h5"
+            component="h2">
+ 
+             {msg.username}:{msg.text}
+
+            </Typography>
+        </CardContent>
+
+        </Card>
+        </div>
+    )
+}
+
+export default Messages
