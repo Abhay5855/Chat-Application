9f3c0fe5074a6e84a9d32288dd797c24a9c638a9
Abhay5855 <ap3919529@gmail.com>
2021-05-06 18:28:28 +0530

    docs: Adding username and the message as objects

--- chat-webapp-react/src/components/Chat.js
+++ chat-webapp-react/src/components/Chat.js
@@ -1,23 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
 import { FormControl } from "@material-ui/core";
 import { InputLabel } from "@material-ui/core";
 import { Input } from "@material-ui/core";
+import Messages from "../components/Messages";
 
 function Chat() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {username:'Abhay' , text:'Loves Heena'},
+    {username:'Heena' , text:'Loves Abhay'}
+  ]);
+  const [username, setUsername] = useState("");
+
+
+ useEffect(() => {
+     
+  setUsername(prompt('Enter your name'))
+ },[])
+
+  
 
   const sendMessage = (e) => {
     e.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text:input}]);
     setInput("");
   };
 
-  console.log(input);
-  console.log(messages);
+ 
   return (
     <Container>
       <form>
@@ -37,7 +49,7 @@ function Chat() {
       </form>
 
       {messages.map((msg) => {
-        return <p>{msg}</p>;
+        return <Messages username={msg.username}  text={msg.text} />;
       })}
     </Container>
   );
