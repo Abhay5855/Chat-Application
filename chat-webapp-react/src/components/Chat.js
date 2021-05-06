8542a56fcd7c1a243567949bb6e3626fccac6ab7
Abhay5855 <ap3919529@gmail.com>
2021-05-06 10:45:57 +0530

    docs: Using map to displaying the messages

--- chat-webapp-react/src/components/Chat.js
+++ chat-webapp-react/src/components/Chat.js
@@ -1,44 +1,38 @@
-import React from 'react'
+import React from "react";
 // import { useState } from "react";
 import styled from "styled-components";
-import {useState} from "react";
+import { useState } from "react";
 function Chat() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(['hello heena','fuck']);
 
+  const sendMessage = (e) => {
+    setMessages([...messages, input]);
+    setInput("");
+  };
 
-    const [input,setInput] = useState('');
-    const [messages,setMessages] = useState([]);
-
-
-    const sendMessage = (e) => {
-        setMessages([...messages,input]);
-        setInput('');
-    }
-
-    console.log(input);
-    console.log(messages);
-    return (
+  console.log(input);
+  console.log(messages);
+  return (
+    <Container>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={sendMessage}>Send Text</button>
         
-        <Container>
-           <input value={input} onChange={e => setInput(e.target.value)}/>
-           <button onClick={sendMessage}>Send Text</button>
-        </Container>
+     {
+         messages.map((msg)=> {
+             return (
+                 <p>{msg}</p>
+             )
+         })
+     }
        
+    </Container>
+      
 
 
-            
-        
-    )
+  );
 }
-
 
 const Container = styled.div``;
 
-
-
-
-
-
-
-
-
-export default Chat
+export default Chat;
