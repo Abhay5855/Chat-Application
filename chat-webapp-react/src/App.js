40556cd79a07f65b56f30a555e92cd512e699245
Abhay5855 <ap3919529@gmail.com>
2021-05-07 21:22:06 +0530

    fix: Removing the router

--- chat-webapp-react/src/App.js
+++ chat-webapp-react/src/App.js
@@ -1,20 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import styled from "styled-components";
 function App() {
   return (
     <div className="App">
-      <Router>
+      
         <Container>
-          <Switch>
-            <Route path="/room">
+         
               <Chat />
-            </Route>
-            <Route path="/">Login</Route>
-          </Switch>
+            
         </Container>
-      </Router>
+     
     </div>
   );
 }
