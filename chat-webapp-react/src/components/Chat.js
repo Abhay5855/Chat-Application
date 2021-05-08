3f9e9111a51bc6777167a556188a799ec4c7b986
Abhay5855 <ap3919529@gmail.com>
2021-05-08 19:24:31 +0530

    feat: Using useParams hook to route through id

--- chat-webapp-react/src/components/Chat.js
+++ chat-webapp-react/src/components/Chat.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Chat.css';
+import { useParams } from 'react-router-dom'
 function Chat() {
+
+    const { roomId } = useParams();
     return (
-        <div>
+        <div className="chat">
+
+        <h2>You are in {roomId} room</h2>
             
         </div>
     )
