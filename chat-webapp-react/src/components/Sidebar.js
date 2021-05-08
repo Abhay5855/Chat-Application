6b7892ce5ae0635892722a84d30c426492cd8818
Abhay5855 <ap3919529@gmail.com>
2021-05-08 13:39:59 +0530

    docs:Adding the user and other ifo

--- chat-webapp-react/src/components/Sidebar.js
+++ chat-webapp-react/src/components/Sidebar.js
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import "./Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AddIcon from "@material-ui/icons/Add";
+import HomeIcon from "@material-ui/icons/Home";
+import CreateIcon from '@material-ui/icons/Create';
 function Sidebar() {
-    return (
-        <div>
-            
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+       
+        <div className="sidebar__info">
+
+         <h2>Abhay Patil</h2>
+        
+          <h3>
+            <FiberManualRecordIcon />
+          
+            Abhay patil
+          </h3>
+
+         
+          
+
         </div>
-    )
+      
+      </div>
+
+      <div className="sidebar__addchannels">
+        {/* <ArrowDropDownIcon /> */}
+        {/* Channnel */}
+        {/* <h3>Channels</h3> */}
+
+        {/* Addicon */}
+
+        {/* < AddIcon /> */}
+      </div>
+
+      <div className="sidebar__channels">
+        {/* <HomeIcon />
+            <h4>React Help</h4> */}
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
